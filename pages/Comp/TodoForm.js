6fa79ef5f886204864e1455783d67c323efd1742
Nouran-Ali <68993568+Nouran-Ali@users.noreply.github.com>
2022-08-13 15:05:10 +0200

    todo web

---
+++ pages/Comp/TodoForm.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect, useRef } from "react";
+import styles from "../../styles/Form.module.css";
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      {props.edit ? (
+        <div
+          className={`w-100 mx-auto d-flex justify-content-center align-items-center ${styles.form} `}
+        >
+          <div className="w-50">
+          <input
+            placeholder="Update your item"
+            value={input}
+            onChange={handleChange}
+            name="text"
+            ref={inputRef}
+            className={`form-control shadow-none border-0 ${styles.input}`}
+          /><br/>
+          <button onClick={handleSubmit} className={`border-0 p-2 rounded-2 w-100 ${styles.btnAdd}`}>
+            Update
+          </button>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`w-100 mx-auto d-flex justify-content-center align-items-center ${styles.form} `}
+        >
+        <div className="w-50">
+          <input
+            placeholder="Todo"
+            value={input}
+            onChange={handleChange}
+            name="text"
+            className={`form-control shadow-none border-0 ${styles.input}`}
+            ref={inputRef}
+          /><br/>
+          <button onClick={handleSubmit} className={`border-0 p-2 rounded-2 w-100 ${styles.btnAdd}`}>
+            Add todo
+          </button>
+          </div>
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
